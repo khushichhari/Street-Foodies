f15946b2a53a581992576b3de247c51f29d6b287
khushichhari <chharikhushi8@gmail.com>
2025-05-26 16:33:47 +0530

    Updated styles and responsive layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./styles/HomePage.css";
 import HomeGif from "./assets/Home.gif";
 import Food from "./Food.js";
-import About from "./About.js"
+import About from "./About.js";
 import Contact from "./Contact.js";
+import DeliveryGif from "./assets/Delivery.gif";
 
 function HomePage() {
   return (
@@ -22,16 +23,31 @@ function HomePage() {
           <a href="tel:9503208766">
             <button className="btn">Order Now</button>
           </a>
+
           <h3 className="line1"> Timing : 5:00 pm to 9:30pm at evening.
             Free home delivery order up to 50₹.</h3>
           <h3 className="line1"> Chicken handi & Mutton handi orders are accepted here.</h3>
+
+          <h3 className="line1">
+            {" "}
+            Timing : 5:00 pm to 9:30pm at evening. Free home delivery! order up
+            to 50₹.
+          </h3>
+          <h3 className="line1">
+            {" "}
+            Chicken handi & Mutton handi orders are accepted here.
+          </h3>
+        </div>
+        <div className="delivery">
+          {" "}
+          <img src={DeliveryGif} alt="Delivery" className="delivery-gif" />
         </div>
       </div>
-      <Food/>
-      
-      <About/>
+      <Food />
 
-      <Contact/>
+      <About />
+
+      <Contact />
     </>
   );
 }
